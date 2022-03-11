ff96894e5a43f6c314e557e4c84fd00e177e8991
junqi.chen <junqiqi77_chen@foxmail.com>
2022-03-11 15:42:33 +0800

    第一次提交git-xuexi3

---
+++ src/main/java/com/example/gitxuexi3/GitXuexi3Application.java
@@ -0,0 +1,13 @@
+package com.example.gitxuexi3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitXuexi3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitXuexi3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitxuexi3/GitXuexi3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitxuexi3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitXuexi3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
